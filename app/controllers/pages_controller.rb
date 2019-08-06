ed06f9a8eb2c03ed5f9d1dd5dae378e3e518d087
Emmanuel Narkuwah <coding@bigballer.co.uk>
2019-08-06 17:49:49 +0100

    exclude home page from devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  # Uncooment below to allow access to only home page without login can add other pages to array.
-  # skip_before_action :authenticate_user!, only: [:home]
+  #allow access to only home page without login can add other pages to array.
+  skip_before_action :authenticate_user!, only: [:home]
   def home
   end
 end
